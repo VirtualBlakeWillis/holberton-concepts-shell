2390fbd195c0c69dbaa067847f2b918aae44aeb2
VelociraptorJesus <bewillis101@gmail.com>
2022-07-22 10:46:00 -0500

    vs code save

--- concept-shell.c
+++ concept-shell.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
-char **argv;
-char **buffer;
-
 int command(char *cmd);
 int count_args(char *str);
 void tokenize_string(char *buff);
-
+char *prep_string(char *cmd);
 /**
  * main - simple shell
  *
@@ -15,88 +12,22 @@ void tokenize_string(char *buff);
 int main(void)
 {
 	size_t buffsize;
-	char *buff;
+	char *buff, cmd;
 	int i = 0, num_args, x;
-	while(1)
+	while (1)
 	{
 		printf("#concept-shell$ ");
-		
+
 		getline(&buff, &buffsize, stdin);
-		
+
 		if (strcmp(buff, "exit\n") == 0)
 			break;
 		else
 		{
-			num_args = count_args(buff);
-			*argv = malloc(sizeof(char *) * 10);
-			tokenize_string(buff);
-
-			for (x = 0; x < 1; x++)
-				printf("argv[%d]: %s\n", x, argv[x]);
-			/*
-			 * printf("Strlen: %ld\n", strlen(buff));
-			 * printf("Num of Args: %d\n", num_args);
-			 */
+			command(buff);
 		}
 	}
-	free(argv);
 	return (EXIT_SUCCESS);
-}
-/**
- * tokenize_string - populate *argv[] with arguments
- * @buff: command line input from user
- * Return: void
- */
-
-void tokenize_string(char *buff)
-{
-	char *argument;
-	int i = 0;
-	argument = strtok(buff, " ");
-	
-	while (argument != NULL)
-	{
-		argv[i] = argument;
-		argument = strtok(NULL, " \0");
-		i++;
-	}
-}
-
-
-/**
- * count_args - count # of args in string
- * @str: command line input from user
- *
- * Return: Number of args found
- */
-int count_args(char *str)
-{
-	int i = 0, n = 0;
-	char temp;
-
-	while (str[i] == ' ') /* iterates past spaces at start of string*/
-		i++;
-	for(; i < strlen(str); i++)
-	{
-		if (str[i] == ' ' && str[i-1] != ' ' && str[i+1] != ' ')
-			n++;
-		/* arguments wrapped in quotes are counted as 1 argument
-		if (str[i] == '"')
-		{
-			i++;
-			while (str[i] != '"')
-			{
-				if (str[i] == '\0')
-				{
-					printf("Error: unclosed puncuation mark in input\n");
-					return (-1);
-				}
-				i++;
-			}
-		} */
-	}
-	n++;
-	return (n);
 }
 
 /**
@@ -119,7 +50,7 @@ int command(char *cmd)
 	if (my_pid == 0)
 	{
 		/* child */
-		execve(cmd, argv, NULL);
+		execve(argv[0], argv, NULL);
 	}
 	else
 	{
@@ -127,4 +58,76 @@ int command(char *cmd)
 		/* parent */
 	}
 	return (EXIT_SUCCESS);
+}
+
+char *prep_string(char *cmd)
+{
+	int i = 0;
+
+	while (cmd[i] == ' ')
+		i++;
+
+	for (; i < strlen(cmd); i++)
+	{
+		if (cmd[i] == ' ' || cmd[i] == '\0')
+		{
+			cmd[i] = '\0';
+			return (cmd);
+		}
+	}
+	return (cmd);
+}
+/**
+ * tokenize_string - populate *argv[] with arguments
+ * @buff: command line input from user
+ * Return: void
+ */
+
+void tokenize_string(char *buff)
+{
+	char *argument;
+	int i = 0;
+	argument = strtok(buff, " ");
+
+	while (argument != NULL)
+	{
+		argument = strtok(NULL, " \0");
+		i++;
+	}
+}
+
+/**
+ * count_args - count # of args in string
+ * @str: command line input from user
+ *
+ * Return: Number of args found
+ */
+int count_args(char *str)
+{
+	int i = 0, n = 0;
+	char temp;
+
+	while (str[i] == ' ') /* iterates past spaces at start of string*/
+		i++;
+	for (; i < strlen(str); i++)
+	{
+		if (str[i] == ' ' && str[i - 1] != ' ' && str[i + 1] != ' ')
+			n++;
+		/* arguments wrapped in quotes are counted as 1 argument
+		if (str[i] == '"')
+		{
+			i++;
+			while (str[i] != '"')
+			{
+				if (str[i] == '\0')
+				{
+					printf("Error: unclosed puncuation mark in input\n");
+					return (-1);
+				}
+				i++;
+			}
+		} */
+	}
+	n++;
+	return (n);
 }
